71c6624126f1e00a41a3ec537f8b34caaddfec4b
Alex Teixeira <alextx@poli.ufrj.br>
2020-08-09 16:21:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def nao_entre_em_panico():
+def app():
     if request.headers.get('Authorization') == '42':
         return jsonify({"42": "a resposta para a vida, o universo e tudo mais"})
     return jsonify({"message": "Não entre em pânico!"})
